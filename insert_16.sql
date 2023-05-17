0019d41f626abfe44f7bad24760fe5d8026fc694
Owusu2 <118225230+Owusu2@users.noreply.github.com>
2023-05-17 19:22:51 +0100

    Update insert_16.sql

--- insert_16.sql
+++ insert_16.sql
@@ -1,1 +1,26 @@
+INSERT INTO locations (dc_location_id, address)
+VALUES (50000, "NORTHAMPTON");
 
+INSERT INTO instructors(instructor_id, fname, sname, grade, contact details) 
+VALUES (10000, "MANDY" ,"MORRELL", "PASS", "chrismaowusu@gmail.com")
+
+INSERT INTO courses(course_id, lesson_category, course_level, price)
+VALUES (4000, "CSY1030-STD", "COMPUTING","BSc", "£9250")
+
+INSERT INTO lessons (learner_id, reg_number_id, lesson_date, lesson_time_)
+VALUE (5003, "22813920", "KV29 DUI", "12/01/2023", "15:20 PM" )
+
+INSERT INTO learners (learner_id, learner_age, Lfname, Lsname, Learner_contact_no)
+VALUES (10000, "22813290", "18,", "Chrisma", "Owusu", "07521963186")
+
+INSERT INTO licenses (license_id, license_number, licence_type)
+VALUES (10000, "9872127", "849339", "B1")
+
+INSERT INTO driving_tests(driving_test_id, driving_test_date, driving_test_time)
+VALUES (50000, "55555555", "07/06/2022", "10:00")
+
+INSERT INTO stages(stage_element, stage_id, completion)
+VALUES (60000, "finished", "55555555", "07/06/2022")
+
+INSERT INTO tests(test_id, test_category, test_type)
+VALUES ("22813921", "Category B", "Category B" )
