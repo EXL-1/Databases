80347de73142e7131d715013732b071b19f13da3
willtreagus20 <126470246+willtreagus20@users.noreply.github.com>
2023-05-17 16:56:10 +0100

    create table instructors

--- create_16.sql
+++ create_16.sql
@@ -4,3 +4,12 @@ CREATE TABLE locations (
     address VARCHAR2 (20);
 )
 
+CREATE TABLE instructors (
+    instructor_id NUMBER(5),
+    dc_location_id NUMBER(5),
+    firstname VARCHAR2(20),
+    surname VARCHAR2(20),
+    contact VARCHAR2 (5) NOT NULL,
+    grade VARCHAR2 (5);
+
+)
