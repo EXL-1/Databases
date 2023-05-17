58608635ebcf0be8e84e20c947a40f7d10f5dbe4
Lucas Malik <47810442+EXL-1@users.noreply.github.com>
2023-05-17 17:04:08 +0100

    Update queries_66.sql

--- CSY1065/queries_66.sql
+++ CSY1065/queries_66.sql
@@ -1,6 +1,6 @@
 -- CSY 1065_66 Databases Lucas Malik 17/05/2023
 -- @C:\Database\Databases_WorkFiles\scripts\CSY1065\queries_66.sql
--- CSY1065_66@STUDENT/22806287
+-- 
 
 SELECT company_id, company_name, city, training_budget 
 FROM companies 
