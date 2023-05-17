6b07aafa7e349c5b319cd3419d84fbed62692fdc
Owusu2 <118225230+Owusu2@users.noreply.github.com>
2023-05-17 17:04:36 +0100

    Update drop_16.sql

--- drop_16.sql
+++ drop_16.sql
@@ -1,1 +1,27 @@
+DROP TABLE locations;
+DROP TABLE locations PURGE;
+
+DROP TABLE instructors 
+DROP TABLE instructors PURGE;
+
+DROP TABLE courses
+DROP TABLE courses PURGE; 
+
+DROP TABLE lessons 
+DROP TABLE lessons PURGE; 
+
+DROP TABLE learners 
+DROP TABLE learners PURGE; 
+
+DROP TABLE licenses 
+DROP TABLE licenses PURGE; 
+
+DROP TABLE driving_tests 
+DROP TABLE driving_tests PURGE; 
+
+DROP TABLE stages
+DROP TABLE stages_element PURGE; 
+
+DROP TABLE tests 
+DROP TABLE tests PURGE; 
 
