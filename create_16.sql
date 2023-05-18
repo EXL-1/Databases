0cd25f95dda7de1fddc6a36a23c9c9fbcd6b8780
Lucas Malik <47810442+EXL-1@users.noreply.github.com>
2023-05-18 13:14:32 +0100

    Update create_16.sql

--- create_16.sql
+++ create_16.sql
@@ -8,7 +8,7 @@ CREATE TABLE locations (
 
 CREATE TABLE instructors (
     instructor_id NUMBER(5),
-    dc_location_id NUMBER(5),
+    dc_location_id NUMBER(5) NOT NULL,
     firstname VARCHAR2(20),
     surname VARCHAR2(20),
     contact VARCHAR2(10) NOT NULL,
@@ -17,20 +17,20 @@ CREATE TABLE instructors (
 CREATE TABLE cars (
     car_reg NUMBER(7),
     car_model VARCHAR2(10) NOT NULL,
-    dc_location_id NUMBER(5));
+    dc_location_id NUMBER(5)) NOT NULL;
 
 CREATE TABLE courses (
     course_id NUMBER(5),
     lesson_category VARCHAR2(3),
     course_level VARCHAR2(15),
     price NUMBER(10),
-    instructor_id NUMBER(5));
+    instructor_id NUMBER(5)) NOT NULL;
 
 CREATE TABLE lessons (
     lesson_id NUMBER(5),
     learner_id NUMBER(5),
-    car_reg NUMBER(7) NOT NULL,
-    course_id NUMBER(5),
+    car_reg NUMBER(7) ,
+    course_id NUMBER(5) NOT NULL,
     lesson_date DATETIME );
 
 CREATE TABLE learners (
@@ -55,8 +55,8 @@ CREATE TABLE stages (
     stage_id NUMBER(5),
     stage_element NUMBER(10),
     completion CHAR(1),
-    test_id NUMBER(5),
-    learner_id NUMBER(5));
+    test_id NUMBER(5) NOT NULL,
+    learner_id NUMBER(5) NOT NULL);
 
 CREATE TABLE tests (
     test_id NUMBER(5),
