0432581c85d4fc847f507bf09f80dbf92ff2265b
Lucas Malik <47810442+EXL-1@users.noreply.github.com>
2023-05-18 03:21:22 +0100

    Update drop_16.sql

--- drop_16.sql
+++ drop_16.sql
@@ -1,27 +1,16 @@
+-- DROP TABLES 
+
 DROP TABLE locations;
-DROP TABLE locations PURGE;
+DROP TABLE instructors;
+DROP TABLE cars;
+DROP TABLE courses;
+DROP TABLE lessons; 
+DROP TABLE learners; 
+DROP TABLE licenses; 
+DROP TABLE driving_tests; 
+DROP TABLE test;
+DROP TABLE stages;
 
-DROP TABLE instructors 
-DROP TABLE instructors PURGE;
+PURGE RECYCLEBIN;
 
-DROP TABLE courses
-DROP TABLE courses PURGE; 
-
-DROP TABLE lessons 
-DROP TABLE lessons PURGE; 
-
-DROP TABLE learners 
-DROP TABLE learners PURGE; 
-
-DROP TABLE licenses 
-DROP TABLE licenses PURGE; 
-
-DROP TABLE driving_tests 
-DROP TABLE driving_tests PURGE; 
-
-DROP TABLE stages
-DROP TABLE stages_element PURGE; 
-
-DROP TABLE tests 
-DROP TABLE tests PURGE; 
 
